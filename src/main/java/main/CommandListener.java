09804d04d299436a9a914ac6f204c99a4a225795
BrendanLeeGit <brendanlee09090@gmail.com>
2024-06-11 03:48:32 -0400

    Removed unneeded import statement.

--- src/main/java/main/CommandListener.java
+++ src/main/java/main/CommandListener.java
@@ -5,7 +5,6 @@ import commands.Invoker;
 import commands.RegisterUserCommand;
 import commands.SendScreenShotCommand;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
