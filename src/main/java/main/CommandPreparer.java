a312cc1a05b9030e3410ca1c3e8a5939dc28f035
BrendanLeeGit <brendanlee09090@gmail.com>
2024-06-11 21:23:23 -0400

    Added CommandPreparer as a separate class for registering and executing commands.

---
+++ src/main/java/main/CommandPreparer.java
@@ -0,0 +1,35 @@
+package main;
+
+import commands.Invoker;
+import commands.RegisterUserCommand;
+import commands.SendScreenShotCommand;
+import receivers.GeneralReceiver;
+
+public class CommandPreparer {
+    private MessageChannelTracker messageChannelTracker;
+    private Invoker invoker;
+    private GeneralReceiver receiver;
+
+    public CommandPreparer(MessageChannelTracker messageChannelTracker){
+        this.messageChannelTracker = messageChannelTracker;
+        invoker = new Invoker();
+    }
+
+    public void prepareCommands(){
+        generateReceivers();
+        registerCommands();
+    }
+
+    private void generateReceivers(){
+        receiver = new GeneralReceiver(messageChannelTracker);
+    }
+
+    private void registerCommands(){
+        invoker.registerCommand("SendScreenshot", new SendScreenShotCommand(receiver));
+        invoker.registerCommand("RegisterUser", new RegisterUserCommand(receiver));
+    }
+
+    public void processInputtedCommand(String[] input){
+        invoker.executeCommand(input);
+    }
+}
